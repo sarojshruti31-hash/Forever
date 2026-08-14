4d96d981aa6f6b5fca8c7580fd8133f0bceb0f96
Shruti Saroj <shruti09@gmail.com>
2026-08-14 16:19:15 +0530

    Fix req.body initialization in authUser middleware for GET requests

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,7 +7,11 @@ const authUser = async (req, res, next) => {
             return res.json({ success: false, message: "Not Authorized Login Again" })
         }
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+        if (!req.body) {
+            req.body = {}
+        }
         req.body.userId = token_decode.id
+        req.userId = token_decode.id
         next()
     } catch (error) {
         console.log(error)
